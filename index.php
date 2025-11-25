3371bd85c36c0d53d1e70aa7dc1e1785ae7e54e0
SAndrew-Artur21 <arturandrew723@gmail.com>
2025-11-24 21:21:48 -0300

    Estiliza a pagina index

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ $games = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <title>Loja de Jogos</title>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="assets/css/global.css">
-  <link rel="stylesheet" href="assets/css/cards.css">
+  <link rel="stylesheet" href="css/global.css">
+  <link rel="stylesheet" href="css/cards.css">
 </head>
 <body>
 
